94e5eccbcfb40defac7afcef9351b93b021d9a0d
Steffano <smalagadev@gmail.com>
2019-12-12 10:06:57 -0500

    Project 0 initialized, Main menu options instantiated

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,10 +1,28 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Yoooooo, what up player, welcome to the bank of the streets.");
+		Scanner in = new Scanner(System.in);
+		int option = 0;
+		
+		
 
+		do {
+			System.out.println("So, whatchu tryna do? Hit them digits to pick ya potion.");
+			System.out.println("1. Let's get this money");
+			System.out.println("2. Ball out");
+			System.out.println("3. Oooooh flex on em.");
+			System.out.println("4. Get back out there and make that paper");
+			
+			option = in.nextInt();
+		}while(option != 4);
+		
+		System.out.println("Aight, I'll catch you later.");
+		in.close();
 	}
 
 }
