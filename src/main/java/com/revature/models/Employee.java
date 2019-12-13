4db94d63e0db4cf4a77476894b5bbbdb0066673d
Steffano <smalagadev@gmail.com>
2019-12-13 10:16:47 -0500

    New class created and instantiated

---
+++ src/main/java/com/revature/models/Employee.java
@@ -0,0 +1,13 @@
+package com.revature.models;
+
+public class Employee extends User {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int option = 0;
+		private int employeeId;
+		private String first_name;
+		private String last_name;
+		private String username;
+
+	}
+}
