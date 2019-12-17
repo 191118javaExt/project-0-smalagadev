ec9f0894525acc20871a6fba10630b226179a386
Steffano <smalagadev@gmail.com>
2019-12-17 15:22:46 -0500

    Connection util reverted to original state and Logger.INFO added

--- src/main/java/com/revature/util/ConnectionUtil.java
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -2,10 +2,9 @@ package com.revature.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class ConnectionUtil {
@@ -21,12 +20,7 @@ public class ConnectionUtil {
 		
 		try{
 			conn = DriverManager.getConnection(url, username, password);
-			System.out.println("Connected to database.");
-			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM public.employee");
-            while (resultSet.next()) {
-                System.out.printf( resultSet.getString("first_name") + " " + resultSet.getString("last_name") + "\n");
-            }
+			logger.log(Level.INFO, "Connected to database.");
 		} catch (SQLException e) {
 			logger.warn("Unable to connect to database.", e);
 		}
