eba40b3355225c78bb9c42d4cbc781e52967c679
Steffano <smalagadev@gmail.com>
2019-12-17 13:35:35 -0500

    Reverted back to original set up

--- src/main/java/com/revature/util/ConnectionUtil.java
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -2,7 +2,9 @@ package com.revature.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.log4j.Logger;
 
@@ -11,7 +13,7 @@ public class ConnectionUtil {
 	private static Logger logger = Logger.getLogger(ConnectionUtil.class);
 	
 	public static Connection getConnection() {
-		String url = "jdbc:postgresql://localhost:5432/";
+		String url = "jdbc:postgresql://localhost:5432/postgres";
 		String username = "postgres";
 		String password = "password";
 		
@@ -19,6 +21,12 @@ public class ConnectionUtil {
 		
 		try{
 			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected to database.");
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM public.employee");
+            while (resultSet.next()) {
+                System.out.printf( resultSet.getString("first_name") + " " + resultSet.getString("last_name") + "\n");
+            }
 		} catch (SQLException e) {
 			logger.warn("Unable to connect to database.", e);
 		}
