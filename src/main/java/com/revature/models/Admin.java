310a8e41abdfbd343ea9e228c26da7f7a3b4ce83
Steffano <smalagadev@gmail.com>
2019-12-17 22:07:09 -0500

    Overloaded constructor created

--- src/main/java/com/revature/models/Admin.java
+++ src/main/java/com/revature/models/Admin.java
@@ -5,5 +5,9 @@ public class Admin extends Employee {
 	public Admin() {
 		super();
 	}
-
+	
+	public Admin(int employeeId, String first, String last, String user) {
+		super(employeeId, first, last, user);
+		
+	}
 }
