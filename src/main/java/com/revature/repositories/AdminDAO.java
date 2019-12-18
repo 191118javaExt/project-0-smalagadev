ebdf72d8cf993b1bb10cf5b45baea342e247d502
Steffano <smalagadev@gmail.com>
2019-12-17 22:14:43 -0500

    Password parameter add to insert()

--- src/main/java/com/revature/repositories/AdminDAO.java
+++ src/main/java/com/revature/repositories/AdminDAO.java
@@ -7,6 +7,6 @@ import com.revature.models.Admin;
 public interface AdminDAO {
 	public List<Admin> findAll();
 	public Admin findById(int id);
-	public boolean insert(Admin a);
+	public boolean insert(Admin a, String password);
 	public boolean update(Admin a);
 }
