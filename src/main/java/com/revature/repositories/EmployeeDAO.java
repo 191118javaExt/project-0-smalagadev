8250e07d7c42af82de32218905de8ecf8fa284d6
Steffano <smalagadev@gmail.com>
2019-12-17 21:56:08 -0500

    User import removed

--- src/main/java/com/revature/repositories/EmployeeDAO.java
+++ src/main/java/com/revature/repositories/EmployeeDAO.java
@@ -3,7 +3,6 @@ package com.revature.repositories;
 import java.util.List;
 
 import com.revature.models.Employee;
-import com.revature.models.User;
 
 public interface EmployeeDAO {
 	public List<Employee> findAll();
