7c81b4706da8737b7b1224ce0b949ea13bf67c14
Steffano <smalagadev@gmail.com>
2019-12-19 15:31:34 -0500

    Main menu completed

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -2,31 +2,72 @@ package com.revature;
 
 import java.util.Scanner;
 
+import com.revature.services.*;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Yoooooo, what up player, welcome to the bank of the streets.");
+		AdminService as = new AdminService();
+		EmployeeService es = new EmployeeService();
+		UserService us = new UserService();
+		
+		System.out.println("Welcome to your online Banking.\n");
 		Scanner in = new Scanner(System.in);
-		int option = 0;
+		String option; 
+		boolean running = true;
+		// Default Texts
+		String divider = "\n===================";
+		String optionText = "\nSelect from you options:\n";
 		
 
+		
 		do {
-			System.out.println("So, whatchu tryna do? Hit them digits to pick ya potion.");
-			System.out.println("1. Let's get this money");
-			System.out.println("2. Ball out");
-			System.out.println("3. Oooooh flex on em.");
-			System.out.println("4. Get back out there and make that paper");
-			try {
-				option = in.nextInt();
+			System.out.println(divider);
+			System.out.println(optionText);
+			System.out.println("1. Employee Login");
+			System.out.println("2. Customer Login");
+			System.out.println("3. Sign up");
+			System.out.println("4. Exit");
+			
+		
+			try{
+				option = in.nextLine();
+				switch(option) {
+				case "1":
+					System.out.println(divider);
+					System.out.println("Employee Login");
+					es.login();
+					es.options();
+					break;
+				case "2":
+					System.out.println(divider);
+					System.out.println("Customer Login");
+					us.login();
+					us.options();
+					break;
+				case "3":
+					System.out.println(divider);
+					System.out.println("Sign up");
+					us.signup();
+					break;
+				case "4":
+
+					running = false;
+					break;
+				default:
+					System.out.println("Use the following options above to make a selection.");
+				}
 			}
-			catch (InputMismatchException e) {
+			catch (Exception e) {
+				System.out.println("Please enter a valid number to make a selection.");
 				
 			}
 			
-		}while(option != 4);
+		}while(running);
 		
-		System.out.println("Aight, I'll catch you later.");
 		in.close();
+		System.out.println(divider);
+		System.out.println("\nThank you for using our service.");
 	}
 
 }
