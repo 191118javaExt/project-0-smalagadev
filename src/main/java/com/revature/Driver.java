7a0da0698b02908c11f8ad92e86c03dd4cf31c32
Steffano <smalagadev@gmail.com>
2019-12-21 11:15:53 -0500

    Admin & Employer login implemented

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -2,6 +2,9 @@ package com.revature;
 
 import java.util.Scanner;
 
+import com.revature.models.Admin;
+import com.revature.models.Employee;
+import com.revature.models.User;
 import com.revature.services.*;
 
 public class Driver {
@@ -10,6 +13,9 @@ public class Driver {
 		AdminService as = new AdminService();
 		EmployeeService es = new EmployeeService();
 		UserService us = new UserService();
+		Admin a;
+		Employee e;
+		User u;
 		
 		System.out.println("Welcome to your online Banking.\n");
 		Scanner in = new Scanner(System.in);
@@ -24,10 +30,7 @@ public class Driver {
 		do {
 			System.out.println(divider);
 			System.out.println(optionText);
-			System.out.println("1. Employee Login");
-			System.out.println("2. Customer Login");
-			System.out.println("3. Sign up");
-			System.out.println("4. Exit");
+			System.out.println("1. Employee Login\n2. Customer Login\n3. Sign up\n4. Exit");
 			
 		
 			try{
@@ -36,29 +39,42 @@ public class Driver {
 				case "1":
 					System.out.println(divider);
 					System.out.println("Employee Login");
-					es.login();
-					es.options();
+					e = es.login(); // return values will be updated
+					if(e.getRole().equals("Bank Admin")) {
+						a = new Admin(e);
+						running = as.options(a);
+					}
+					else {
+						running = es.options(e);
+					}
+					
 					break;
+					
 				case "2":
 					System.out.println(divider);
 					System.out.println("Customer Login");
-					us.login();
-					us.options();
+					u = us.login();
+					if(u != null) {
+						running = us.options(u);
+					}	
 					break;
+					
 				case "3":
 					System.out.println(divider);
 					System.out.println("Sign up");
 					us.signup();
 					break;
+					
 				case "4":
 
 					running = false;
 					break;
+					
 				default:
 					System.out.println("Use the following options above to make a selection.");
-				}
+				} 
 			}
-			catch (Exception e) {
+			catch (Exception ex) {
 				System.out.println("Please enter a valid number to make a selection.");
 				
 			}
@@ -68,6 +84,7 @@ public class Driver {
 		in.close();
 		System.out.println(divider);
 		System.out.println("\nThank you for using our service.");
+		System.out.println(divider);
 	}
 
 }
