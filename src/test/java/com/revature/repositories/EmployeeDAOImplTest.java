354636218db8b643eec9bcf4cf9880178f8bdc72
Steffano <smalagadev@gmail.com>
2019-12-23 11:05:15 -0500

    Tests initiated

---
+++ src/test/java/com/revature/repositories/EmployeeDAOImplTest.java
@@ -0,0 +1,19 @@
+package com.revature.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeeDAOImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/revature/util/ConnectionUtilTest.java
@@ -0,0 +1,19 @@
+package com.revature.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConnectionUtilTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
